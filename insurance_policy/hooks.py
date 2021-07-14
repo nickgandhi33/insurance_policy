fabfb7bde87ce73eb3bc0dd044647c18f41c80f3
Nick <gandhi.nikhil3@gmail.com>
2021-07-14 19:11:09 +0530

    first commit

--- insurance_policy/hooks.py
+++ insurance_policy/hooks.py
@@ -8,6 +8,7 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "gandhhi.nikhil3@gmail.com"
 app_license = "MIT"
+fixtures= ["Custom Field"]
 
 # Includes in <head>
 # ------------------
