c7c94d71a1d331612df6aa93f27cfe6addb88528
Nick <gandhi.nikhil3@gmail.com>
2021-07-14 19:35:16 +0530

    Doctypes

---
+++ insurance_policy/insurance_policy/doctype/insurance/insurance.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Nick and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Insurance(Document):
+	pass
---
+++ insurance_policy/insurance_policy/doctype/insurance/test_insurance.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Nick and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestInsurance(unittest.TestCase):
+	pass
